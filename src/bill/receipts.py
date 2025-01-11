27d665874ec39fc79bc4a6f47e948bfafbe4d012
Sung Cho <sung.w.cho@pm.me>
2025-01-10 18:54:17 -0800

    test openai

--- src/bill/receipts.py
+++ src/bill/receipts.py
@@ -3,108 +3,62 @@ import os
 import base64
 import re
 from typing import Iterable
-from huggingface_hub import InferenceClient
-from pydantic import BaseModel, PositiveInt, TypeAdapter
+from openai import OpenAI
+from pydantic import BaseModel
 from typing import Optional
 from logging import getLogger
 
 log = getLogger(__file__)
 
-HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
-HUGGINGFACEHUB_MODEL = "meta-llama/Llama-3.2-11B-Vision-Instruct"
+INFERENCE_API_TOKEN = os.environ["INFERENCE_API_TOKEN"]
+INFERENCE_MODEL = "gpt-4o-mini"
+
 
 class Item(BaseModel):
     name: str
-    count: PositiveInt = 1
+    count: int
     price: float
 
-class Subtotal(Item):
-    name: str = "Subtotal"
+    def __str__(self):
+        return f"{self.name} ({self.count}) : {self.price}"
 
-def read_receipt(receipt_png_data: bytes) -> Iterable[str]:
-    client = InferenceClient(api_key=HUGGINGFACEHUB_API_TOKEN)
+class Items(BaseModel):
+    items: list[Item]
+
+def get_items(receipt_png_data: bytes) -> Iterable[Item]:
+    client = OpenAI(api_key=INFERENCE_API_TOKEN)
 
     image = base64.b64encode(receipt_png_data).decode("utf-8")
-    image = f"data:image/png;base64,{image}"
+    image_url = f"data:image/png;base64,{image}"
 
-    messages = [
+    messages=[
+        {
+            "role": "developer",
+            "content": "You extract items from restaurant receipt images.",
+        },
         {
             "role": "user",
             "content": [
                 {
                     "type": "text",
-                    "text":
-                        "Give me the list of every price and the corresponding name and count in this receipt image. "
-                        "I want the list like this: "
-                        "- name 1, count 1, price 1. "
-                        "- name 2, count 2, price 2. "
+                    "text": "Give me the list of item names, their counts and their prices.",
                 },
                 {
                     "type": "image_url",
                     "image_url": {
-                        "url": image
+                        "url": image_url,
                     }
-                }
+                },
             ]
         }
     ]
 
-    completion = client.chat.completions.create(
-        model=HUGGINGFACEHUB_MODEL, 
-        messages=messages, 
-        max_tokens=512,
-        stream=True,
+    response_format=Items
+
+    response = client.beta.chat.completions.parse(
+        model=INFERENCE_MODEL,
+        messages=messages,
+        response_format=response_format,
     )
 
-    replies = map(lambda chunk: chunk.choices[0].delta.content, completion)
-    yield from replies
-
-def parse_item(receipt_line: str) -> Optional[Item]:
-    receipt_line = receipt_line.strip()
-    log.info(f"image description: {receipt_line}")
-    print(f"image description: {receipt_line}")
-
-    if not receipt_line.startswith("- "):
-        return None
-
-    fields = receipt_line.rsplit(",", 2)
-    fields = [field.strip() for field in fields]
-    fields[0] = fields[0][2:]
-    fields[-1] = fields[-1].replace("$", "")
-    keys = ["name", "count", "price"]
-
-    try:
-        return TypeAdapter(Item).validate_python(dict(zip(keys, fields)))
-    except Exception:
-        return None
-
-def get_lines(words: Iterable[str]) -> Iterable[str]:
-    split_words = map(lambda word: word.splitlines(keepends=True), words)
-    split_words = chain.from_iterable(split_words)
-    re_eol = re.compile(r"[\r\n]+")
-
-    line = []
-    for word in split_words:
-        contains_eol = re_eol.search(word) is not None
-        line.append(re.sub(re_eol, "", word))
-
-        if contains_eol:
-            print(line)
-            yield "".join(line)
-            line = []
-
-    if any(line):
-        yield "".join(line)
-
-def get_items(receipt_png_data: bytes) -> Iterable[Item]:
-    receipt_description = read_receipt(receipt_png_data)
-    description_lines = get_lines(receipt_description)
-    lines = map(parse_item, description_lines)
-    items = filter(None, lines)
-
-    subtotal = 0.0
-    for item in items:
-        subtotal += item.price
-        yield item
-
-    yield Subtotal(price=subtotal)
+    yield from response.choices[0].message.parsed.items
--- tests/test_receipts.py
+++ tests/test_receipts.py
@@ -1,45 +1,25 @@
-from bill.receipts import parse_item, get_items, Subtotal, get_lines, read_receipt
+import math
+from bill.receipts import get_items, Item
 from bill.images import load_image, get_image_data, compress
 from .utils import TEST_DATA_DIR
 import pytest
 
-# @pytest.fixture(scope="session")
-# def receipt_description() -> list[str]:
-#     image_file_path = TEST_DATA_DIR / "20241128_183627.jpg"
-#     image_data = load_image(image_file_path)
-#     description = read_receipt(image_data)
-#     description = list(description)
-#     return description
+@pytest.fixture(scope="session")
+def receipt_items() -> list[Item]:
+    image_file_path = TEST_DATA_DIR / "20241128_183627.jpg"
+    image_data = load_image(image_file_path)
+    items = list(get_items(image_data))
 
-@pytest.fixture()
-def sample_receipt_description() -> list[str]:
-    return """- GL-Domaine Amido Cotes Du Rhone, 1, $13.00
-- Bumble Bee Cooler, 1, $14.00
-- Chilli Gobhi, 1, $12.00
-- Makai Bhel Tart, 1, $12.00
-- Herbed Kulcha, 1, $6.00
-- Cheese Kulcha, 1, $7.00
-- Kala Tikka Murg, 1, $23.00
-- Kasundi Jhinga, 1, $25.00
-- Bengali Kati Roll, 1, $18.00
-- Malwani Fish, 1, $28.00
-- Anjeer Kofta, 1, $24.00
-- Tiffin Box - Chicken, 2, $62.00
-- Amritsari Lamb Chops, 1, $38.00
-- Mango Kulfi, 1, $15.00
-- Shahi Tukda, 1, $15.00
-- Jus d Manguir, 1, $9.00
+    for item in items:
+        print(item)
 
-Note: I use the a.m.d subscript for the list item number if there are more than one item of the same food name. In our list it happens in the food called Tiffin Box - Chicken, where we are selling 2 items instead of just 1.""".splitlines()
+    return items
 
-def test_parse_item(sample_receipt_description):
-    assert parse_item(sample_receipt_description[0])
-    assert not parse_item(sample_receipt_description[-1])
-    assert parse_item(sample_receipt_description[11]).count == 2
+def test_item_counts(receipt_items):
+    assert len(receipt_items) == 16
+    assert receipt_items[11].count == 2
 
-@pytest.mark.parametrize("receipt_file_name", ["20241128_183627.jpg"])
-def test_receipt_items(receipt_file_name):
-    items = list(get_items(load_image(TEST_DATA_DIR / receipt_file_name)))
-    assert len(items) == 17
-    assert isinstance(items[-1], Subtotal)
-    assert items[-1].price == 321.0
+def test_subtotal(receipt_items):
+    prices = [item.price for item in receipt_items]
+    subtotal = sum(prices)
+    assert math.isclose(subtotal, 321, abs_tol=0.01)
