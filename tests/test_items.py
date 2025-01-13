d12d3f313627d61120d00041a49de5f58dd4b791
Sung Cho <sung.w.cho@pm.me>
2025-01-12 17:46:13 -0800

    more item split test

--- tests/test_items.py
+++ tests/test_items.py
@@ -28,5 +28,9 @@ def test_item_split(name: str, split_item_price: float | None):
 @pytest.mark.parametrize("index,split_item_price", [(0, None), (11, 31.0)])
 def test_items_split(index: int, split_item_price: float | None):
     items = deepcopy(EXPECTED_ITEMS)
+    next_item = items.items[index + 1]
     items.split(index)
     assert split_item_price is None or items.items[index + 1].price == split_item_price
+
+    next_item_index = index + 1 if split_item_price is None else index + 2
+    assert items.items[next_item_index] == next_item
