48a20e076747923bc1b440a0b06448451ea192bd
Sung Cho <sung.w.cho@pm.me>
2025-01-31 20:56:20 -0800

    add person works

--- src/ui/items.ts
+++ src/ui/items.ts
@@ -26,7 +26,7 @@ function updateTotal(): void {
     updateSubtotal();
     updateExtra("tax");
     updateExtra("tip");
-    
+
     const table = document.getElementById("receipt-table") as HTMLTableElement;
     const subtotalCell = table.querySelector('td#subtotal') as HTMLTableCellElement;
     const taxCell = table.querySelector(`td#tax`) as HTMLTableCellElement;
@@ -37,50 +37,108 @@ function updateTotal(): void {
     totalCell.textContent = total.toFixed(2);
 }
 
+function getNumItems(): number {
+    const table = document.getElementById("receipt-table") as HTMLTableElement;
+    const numItems = table.querySelectorAll('button[id^="item-name-"]').length;
+    return numItems;
+}
+
 function addItem(name: string): void {
     const table = document.getElementById("receipt-table") as HTMLTableElement;
-    const tbody = table.querySelector('tbody') as HTMLTableSectionElement;
+    const numItems = getNumItems();
+    const newRowIndex = numItems + 1;
 
     const count = 1;
     const price = 0.00;
 
-    const newRow = tbody.insertRow(-1);
+    const newRow = table.insertRow(newRowIndex);
+    newRow.classList.add("py-1");
+
     const nameCell = newRow.insertCell(0);
     const countCell = newRow.insertCell(1);
     const priceCell = newRow.insertCell(2);
 
-    nameCell.innerHTML = `<button id="item-name-${table.rows.length - 2}">${name}</button>`;
+    nameCell.innerHTML = `<button id="item-name-${numItems}">${name}</button>`;
 
     countCell.textContent = count.toString();
     countCell.classList.add("text-center");
 
-    priceCell.id = `item-price-${table.rows.length - 2}`;
+    priceCell.id = `item-price-${numItems}`;
     priceCell.textContent = price.toFixed(2);
     priceCell.classList.add("text-right");
 
     updateTotal();
 }
 
+function addPerson(name: string): void {
+    const table = document.getElementById("receipt-table") as HTMLTableElement;
+    const headerRow = table.rows[0];
+    const numItems = getNumItems();
+    const numColumns = headerRow.querySelectorAll("td").length;
+    const personIndex = numColumns - 3;
+
+    const nameCell = headerRow.insertCell(numColumns);
+    nameCell.classList.add("text-center");
+    nameCell.textContent = name;
+
+    for (let index = 0; index < numItems; index++) {
+        const checkbox = document.createElement("input");
+        checkbox.id = `person-${personIndex}-check-${index}`;
+        checkbox.type = "checkbox";
+        checkbox.classList.add("inline-block");
+
+        const checkCell = table.rows[index + 1].insertCell(-1);
+        checkCell.classList.add("justify-center", "items-center", "text-center");
+        checkCell.appendChild(checkbox);
+    }
+
+    const extraTypes: string[] = ["subtotal", "tax", "tip", "total"];
+    extraTypes.forEach((extraType, index) => {
+        const rowIndex = index + numItems + 1;
+        const extraCell = table.rows[rowIndex].insertCell(numColumns);
+        extraCell.classList.add("text-right");
+        extraCell.id = `person-${personIndex}-${extraType}`;
+        extraCell.textContent = "0.00";
+    });
+}
+
+function addToTable(itemType: string, doAdd: (name: string) => void): void {
+    const nameInput = document.getElementById(`${itemType}-name`) as HTMLInputElement;
+    const name = nameInput.value.trim();
+
+    if (name) {
+        doAdd(name);
+    }
+
+    const addModal = document.getElementById(`add-${itemType}-modal`) as HTMLDivElement;
+    addModal.classList.add("hidden");
+    nameInput.value = "";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     updateTotal();
+
+    const addPersonButton = document.getElementById("add-person-button") as HTMLButtonElement;
+    const addPersonModal = document.getElementById("add-person-modal") as HTMLDivElement;
+    const addPersonOkButton = document.getElementById("add-person-ok-button") as HTMLButtonElement;
 
     const addItemButton = document.getElementById("add-item-button") as HTMLButtonElement;
     const addItemModal = document.getElementById("add-item-modal") as HTMLDivElement;
     const addItemOkButton = document.getElementById("add-item-ok-button") as HTMLButtonElement;
+
+    addPersonButton.addEventListener("click", () => {
+        addPersonModal.classList.remove("hidden");
+    });
+
+    addPersonOkButton.addEventListener("click", () => {
+        addToTable("person", addPerson);
+    });
 
     addItemButton.addEventListener("click", () => {
         addItemModal.classList.remove("hidden");
     });
 
     addItemOkButton.addEventListener("click", () => {
-        const itemNameInput = document.getElementById("item-name") as HTMLInputElement;
-        const itemName = itemNameInput.value.trim();
-        
-        if (itemName) {
-            addItem(itemName);
-        }
-        
-        addItemModal.classList.add("hidden");
-        itemNameInput.value = ""; // Clear the input field
+        addToTable("item", addItem);
     });
 });
