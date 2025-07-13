d48c6f486dc59abfe8b56aabeda3ff939bcf6114
Sung Cho <sung.w.cho@pm.me>
2025-07-12 23:03:11 -0700

    keep session data in sessions

--- src/ui/session_data.py
+++ src/ui/session_data.py
@@ -1,43 +1,35 @@
 from pathlib import Path
 from bill.receipts import Items
+from bill.person import Person
+from logging import getLogger
 import os
 import json
 
+log = getLogger(__file__)
 
 DATA_DIRECTORY = "data_directory"
 IMAGE_FILE = "image_file"
 ITEMS_FILE = "items_file"
 EXTRAS_FILE = "extras_file"
 PERSONS_FILE = "persons_file"
-SELECTIONS_FILE = "selections_file"
-PEOPLE = "people"
-CURRENT_PERSON = "current_person"
-TAX_PERCENT = "tax_percent"
-TIP_PERCENT = "tip_percent"
 
 
 def start_new_receipt(session: dict):
-    for file in (IMAGE_FILE, ITEMS_FILE, EXTRAS_FILE, PERSONS_FILE, SELECTIONS_FILE):
+    for file in (IMAGE_FILE, ITEMS_FILE, EXTRAS_FILE, PERSONS_FILE):
         try:
             os.remove(session_item_path(session, file))
         except FileNotFoundError:
             pass
 
-    for key in (
+    keys_to_clear = [
         IMAGE_FILE,
         ITEMS_FILE,
         EXTRAS_FILE,
         PERSONS_FILE,
-        SELECTIONS_FILE,
-        PEOPLE,
-        CURRENT_PERSON,
-        TAX_PERCENT,
-        TIP_PERCENT,
-    ):
-        try:
-            del session[key]
-        except KeyError:
-            pass
+    ]
+
+    for key in keys_to_clear:
+        session.pop(key, None)
 
 
 def session_item_path(session: dict, session_item_name: str) -> Path:
@@ -50,3 +42,23 @@ def read_items_file(items_file_path: os.PathLike) -> Items:
 
 def get_receipt_items(session: dict) -> Items:
     return read_items_file(session_item_path(session, ITEMS_FILE))
+
+
+def save_persons_file(persons: list[Person], session):
+    with open(session_item_path(session, PERSONS_FILE), "w") as json_file:
+        json.dump([person.model_dump() for person in persons], json_file, indent=4)
+
+
+def get_current_persons(session: dict) -> list[Person]:
+    try:
+        persons_file = session_item_path(session, PERSONS_FILE)
+
+        with open(persons_file, "r") as json_file:
+            data = json.load(json_file)
+            return [Person(**person_data) for person_data in data]
+
+    except FileNotFoundError:
+        return []
+    except Exception as e:
+        log.warning(f"Error reading persons file: {e}")
+        return []
